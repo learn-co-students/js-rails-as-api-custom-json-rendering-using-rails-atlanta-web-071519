cfe342d0fc8691488128f23a0edaee8125da0449
github username <github email address>
2019-09-05 08:30:13 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,13 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds
+    #no need for instance variables since no longer using ERB
+    #rendering to JSON
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    render json: bird
   end
 end
